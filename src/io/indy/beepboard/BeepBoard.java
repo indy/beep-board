9ea15f4cdaea6016888e8b0f4b811760f7112024
Inderjit Gill <email@indy.io>
2011-01-12 22:46:51 +0000

    copied changes from Sudoku example over to BeepBoard

--- src/io/indy/beepboard/BeepBoard.java
+++ src/io/indy/beepboard/BeepBoard.java
@@ -3,13 +3,71 @@ package io.indy.beepboard;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class BeepBoard extends Activity
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class BeepBoard extends Activity implements OnClickListener
 {
-    /** Called when the activity is first created. */
+    private static final String TAG = "BeepBoard";
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+
     }
+
+    public void onClick(View v)
+    {
+        switch(v.getId()) {
+        case R.id.about_button:
+            Intent i = new Intent(this, About.class);
+            startActivity(i);
+            break;
+            //        case R.id.new_button:
+            //            openNewGameDialog();
+            //            break;
+        case R.id.exit_button:
+            finish();
+            break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId()) {
+        case R.id.settings:
+            startActivity(new Intent(this, Prefs.class));
+            return true;
+        }
+        return false;
+    }
+
+
 }
